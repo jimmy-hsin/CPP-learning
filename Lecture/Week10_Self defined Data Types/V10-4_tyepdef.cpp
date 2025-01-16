a36959fd8c942c3f743563a9bf50c0c6da59b48b
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-16 15:47:01 +0800

    Update V10-4_tyepdef.cpp

--- Lecture/Week10_Self defined Data Types/V10-4_tyepdef.cpp
+++ Lecture/Week10_Self defined Data Types/V10-4_tyepdef.cpp
@@ -5,7 +5,9 @@ using namespace std;
 //相當於幫原本的data type取一個別名，用那個別名包進想要用的格式
 //ex: 原本宣告一個幣值單位 dollar是integer，有天想要把他們都改成float，只要改一個地方就好
 //typedef跟struct一樣，生命週期都只存在於所在的block中而已
-typedef double Dollar;   //通常把type宣告成global variable 比較好用
+
+typedef double Dollar;   //通常把type宣告成global variable 比較好用，
+//若只在main()裡宣告他，那外面的exchange function就會看不懂dollar是什麼，會compilation error
 int exchange(Dollar from, double rate)
 {
     return from*rate;
