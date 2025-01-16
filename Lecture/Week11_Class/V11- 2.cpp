ec1fceb4bb0fdc42af4ed3955e9c3a9e566cb5ef
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-16 16:02:17 +0800

    Update V11- 2.cpp

--- Lecture/Week11_Class/V11- 2.cpp
+++ Lecture/Week11_Class/V11- 2.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+struct MyVector{
+  int n;
+  int* m;
+  void init(int dim);
+  void print();
+}
+void MyVector:: init(int dim){
+  n=dim;
+  m=new int[n];
+  for(int i=0;i<n;i++)
+      m[i]=0;
+}
+
+void MyVector::print(){
+  
+}
