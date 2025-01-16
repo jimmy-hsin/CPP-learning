b57e0a27ea744ed02ba807c52463f1f84a59b41a
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-16 11:58:14 +0800

    Update V10-6_member_function.cpp

--- Lecture/Week10_Self defined Data Types/V10-6_member_function.cpp
+++ Lecture/Week10_Self defined Data Types/V10-6_member_function.cpp
@@ -6,15 +6,16 @@ struct Point
 {
     int x;
     int y;
-    double distToOri()  //內部自訂的function，相當於設定一個自訂變數的性質，
+    //這邊設了一個計算point 到原點距離的 function
+    double distToOri()  //內部自訂的function，相當於設定一個自訂變數的性質
     {
         return sqrt(pow(x,2)+pow(y,2));//sqrt: 取根號 ;   pow(a,b): 回傳a的b次方的值
     }
     //另類寫法:
-    double distOri();   
+    double distOri_2();   
 };
 //另類寫法:
-double Point::distOri()
+double Point::distOri_2()
 {
     return sqrt( pow(x,2) + pow(y,2) );
 }
