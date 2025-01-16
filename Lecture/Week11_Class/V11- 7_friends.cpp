1ec9c7fcf4724423d5fba6a4b16f90d6e92af4a9
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-16 16:53:19 +0800

    Update V11- 7_friends.cpp

--- Lecture/Week11_Class/V11- 7_friends.cpp
+++ Lecture/Week11_Class/V11- 7_friends.cpp
@@ -5,6 +5,7 @@ using namespace std;
 //   *2.Setter: 用來modify private variable的值
 //   *3.Friends: 可以宣告一些global function或者其他class作為friend
 //               ，這些member可以存取class裡的private member
+//   *4. 自己的friend應該自己宣告，為了data hiding 不能宣告自己為別人的friend，也不能讓別人自行宣告為自己的friend，這樣不合理 
 
 class MyVector
 {
