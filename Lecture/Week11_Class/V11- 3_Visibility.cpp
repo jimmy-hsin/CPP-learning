e9b472c440723dbecc7335b086e2e79dbddc1b7f
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-16 16:17:56 +0800

    Update and rename V11- 3.cpp to V11- 3_Visibility.cpp

---
+++ Lecture/Week11_Class/V11- 3_Visibility.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+/*
+  C++中，Class 有三種Visibility:
+  Public : 所有人都能存取
+  Private : 只有類別內部的函數才能存取
+  Protected : 只有其子類別及類別內部可以存取
+
+  如果沒有特別宣告，預設為private
+*/
