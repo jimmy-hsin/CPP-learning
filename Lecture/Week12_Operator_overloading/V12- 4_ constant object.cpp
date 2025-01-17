1fc665a0b0d975ff49fc2c5ab58af03bc64f85fa
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-17 12:09:06 +0800

    Update V12- 4_ constant object.cpp

--- Lecture/Week12_Operator_overloading/V12- 4_ constant object.cpp
+++ Lecture/Week12_Operator_overloading/V12- 4_ constant object.cpp
@@ -15,6 +15,7 @@ class MyVector{
   const function 還是能被一般普通的object呼叫
 */
     void print() const;  
+    bool isEqual(const MyVector& v) const;
 };
 
 MyVector::MyVector():n(0){  //由於n已經被const 宣告了，所以要用member initializer的寫法
@@ -37,12 +38,25 @@ MyVector:: MyVector(const MyVector& v) : n(v.n)
   for(int i = 0; i < n; i++)
     m[i] = v.m[i];
 }
-void MyVector::print() const{ //這邊也要記得補const，因為有可能會有overloading (沒有const)的版本，所以要保持一致才知道你在講誰
+
+void MyVector::print() const{   //這邊也要記得補const，因為有可能會有overloading (沒有const)的版本，所以要保持一致才知道你在講誰
     cout << "(";
     for(int i = 0; i < n - 1; i++)
         cout << m[i] << ", ";
     cout << m[n-1] << ")\n";
 }
+//const 放在最左邊跟放在最右邊意思不一樣，放在最左邊的意思是回傳值是一個constant variable，最右邊代表這是一個constant function
+bool MyVector::isEqual(const MyVector& v) const{    
+    if(n!=v.n)                                
+        return false; 
+    
+    for(int i=0;i<n;i++)
+        if(m[i]!=v.m[i])
+            return false;          
+    return true;
+}
+//要判斷一個function能不能成為constant function很簡單，看看function 裡面有沒有 '=' (賦值)就知道了
+//而其他function操作中我們也要避免 constant variable 被放到 '=' 的左邊
 
 int main()
 {
