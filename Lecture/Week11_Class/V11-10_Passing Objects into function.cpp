7ff70362e9a84c0a66b72fe09454c063f8e8d684
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-17 09:22:28 +0800

    Update V11-10_Passing Objects into function.cpp

--- Lecture/Week11_Class/V11-10_Passing Objects into function.cpp
+++ Lecture/Week11_Class/V11-10_Passing Objects into function.cpp
@@ -22,7 +22,7 @@ class MyVector
     int getM(int i)
         {return m[i];}
    
-    //  --------Passing Value into a function-----------------------------------------
+    //  ----------------------Passing Value into a function-----------------------------------------
     MyVector sum1(MyVector v1,MyVector v2,MyVector v3)        //比較不好，因為這種方法每次呼叫都會需要多新建3個object
     {
         int n=v1.getN();
@@ -33,7 +33,7 @@ class MyVector
         return sumOfVec;
     }
     
-    //  --------Passing object pointer into a function-----------------------------
+    //  ----------------------Passing object pointer into a function-----------------------------
     MyVector sum2(MyVector* v1,MyVector* v2,MyVector* v3)    //稍微好一點，但需要創建3個pointer
     {
         int n=v1->getN();
@@ -43,8 +43,14 @@ class MyVector
         MyVector sumOfVec(n,sov);
         return sumOfVec;
     }
+        /*
+          關於使用object pointer並進行保護的寫法:
+          void myFunction(const MyVector* const ptr)
+            第一個const 是確保ptr指向的物件不被修改，第二個const 是確保ptr本身的內容(即指向的位址不被修改)
+            可以根據自己的需要來做調整
+        */
 
-    //  --------Passing object reference into a function----------------------
+    //  ----------------------Passing object reference into a function----------------------
     MyVector sum3(MyVector& v1,MyVector& v2,MyVector& v3)   //比較好，要什麼值直接去object的位置取。  這個情況下只會建立一個MyVector的object(也就是回傳值sumOfVec)，
     {
         int n=v1.getN();
@@ -55,7 +61,7 @@ class MyVector
         return sumOfVec;
     }
 
-    //  --------Passing constant object reference into a function----------------------
+    //  ----------------------Passing constant object reference into a function----------------------
     MyVector sum4(const MyVector& v1,const MyVector& v2,const MyVector& v3)   //最好，因為我們通常需要確保傳入的物件，他不會被修改，所以用const保護他
     {
         int n=v1.getN();
