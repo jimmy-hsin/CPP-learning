fa3fe2811788a528b0fd01a57c1064a9eff91677
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-17 10:30:15 +0800

    Update and rename V12-1.cpp to V12- 1 _review.cpp

--- Lecture/Week12_Operator_overloading/V12- 1 _review.cpp
+++ Lecture/Week12_Operator_overloading/V12- 1 _review.cpp
@@ -12,48 +12,51 @@ private:
     int n;
     double* m;
 public:
-    //Constructors
+  //Constructors
     MyVector();//default constructor
-    MyVector(int dim,double v[]);
+    MyVector(int dim,double v[]);//self define constructor
     MyVector(const MyVector& v);//copy constructor
-    //destructor
-    ~MyVector();
-    //function
+  //destructor
+    ~MyVector(){
+        delete [] m;
+    }
+  //function
     void print();
     bool isEqual(const MyVector& v);//比較現在這個object跟傳入值v
 };
-
-MyVector::MyVector()
+//如果有協其他constuctor，那系統就不會幫你做default constuctor，所以如果有寫其他constuctor的話，那default constuctor也應該自己定義好
+MyVector::MyVector() //default constuctor
 {   n=0;
-    m=nullptr;              }
-MyVector::MyVector(int dim, double v[])
+    m=nullptr;              
+}
+MyVector::MyVector(int dim, double v[]) //self define constuctor
 {   n=dim;
     m=new double[dim];   //先new出一塊空間，再把V值傳進去，避免shallow copy
     for(int i=0;i<dim;i++)
-        m[i]=v[i];          }
-MyVector::MyVector(const MyVector& v)
+        m[i]=v[i];          
+}
+MyVector::MyVector(const MyVector& v)    //copy constuctor
 {   n=v.n;
     m=new double [n];
     for(int i=0;i<n;i++)
-        m[i]=v.m[i];        }
-
-MyVector::~MyVector()
-{   delete [] m;            }
+        m[i]=v.m[i];        
+}
 
 void MyVector::print()
 {   cout<<"(";
     for(int i=0;i<n-1;i++)
         cout<<m[i]<<",";
-    cout<<m[n-1]<<")\n";    }
+    cout<<m[n-1]<<")\n";    
+}
 
-bool MyVector::isEqual(const MyVector& v)    //如果不用constant reference的話，傳入的值會呼叫copy constructor，會耗費很多資源
-{   if(n!=v.n)                               //加const避免 reference 被改掉
+bool MyVector::isEqual(const MyVector& v){    //如果不用constant reference的話，傳入的值會呼叫copy constructor，會耗費很多資源
+    if(n!=v.n)                               //加const避免 reference 被改掉
         return false;
-    else
-    {   for(int i=0;i<n;i++)
-        {   if(m[i]!=v.m[i])
-                return false;   }       }
-
+    else{   
+        for(int i=0;i<n;i++)
+            if(m[i]!=v.m[i])
+                return false;          
+    }
     return true;
 }
 
