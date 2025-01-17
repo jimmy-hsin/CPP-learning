36fffde13d434f9b13e5c8cf988a8ad75a1b4f5c
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-17 14:57:27 +0800

    Update V9- 5.cpp

--- Lecture/Week 9__C string/V9- 5.cpp
+++ Lecture/Week 9__C string/V9- 5.cpp
@@ -11,9 +11,9 @@ int main()
     print(s);
     return 0;
 }
-void reverse(char p[])
+void reverse(char p[])    //基本上就是傳入一個指標
 {
-    int n=strlen(p);       //取得p的字串長度
+    int n=strlen(p);       //取得p的字串長度，也就是傳入的指標 到 陣列結尾(也就是\0) 的長度
     /* 講義寫法:
     char* temp=new char[n];//新增一個字串 temp
     for(int i=0;i<n;i++)   //將p字串 倒著裝進 temp字串
@@ -25,7 +25,6 @@ void reverse(char p[])
     */
 
     //高效寫法:
-    int n=strlen(p);       //取得p的字串長度，也就是傳入的指標 到 陣列結尾(也就是\0) 的長度
     for(int i=0;i<n/2;i++)   //將p字串 倒著裝進 temp字串
     {
         swap(p[i], p[n-1-i]);
