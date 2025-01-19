4b236fafeb8a8a64eae4a03c4e4dd53fcdb81b71
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-19 17:53:58 +0800

    Update V12- 7_overload of (<)  &  (!=).cpp

--- 1. Lecture/Week12_Operator_overloading/V12- 7_overload of (<)  &  (!=).cpp
+++ 1. Lecture/Week12_Operator_overloading/V12- 7_overload of (<)  &  (!=).cpp
@@ -29,14 +29,12 @@ MyVector:: MyVector(const MyVector& v) : n(v.n){
     for(int i = 0; i < n; i++)
         this->m[i] = v.m[i];
 }
-
 void MyVector::print() const{   
     cout << "(";
     for(int i = 0; i < n - 1; i++)
         cout << this->m[i] << ", ";
     cout << this->m[n-1] << ")\n";
 }
-
 bool MyVector::operator== (const MyVector& v) const{    
     if(this->n!=v.n)                                
         return false; 
@@ -45,12 +43,13 @@ bool MyVector::operator== (const MyVector& v) const{
             return false;          
     return true;
 }
-//我們寫的新的 !=
+
+//~~~~~~~~~~~~~~~~~~~~~~ 我們寫的新的 != ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 bool MyVector::operator!= (const MyVector& v) const{
     return !(*this == v);     //this是指向自己的指標，對她做dereference 就會變成自己
     //我們直接調用 == 的結果，就不用再自己寫一大串
 }
-//我們寫的新的 <
+//~~~~~~~~~~~~~~~~~~~~~~ 我們寫的新的 <  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 bool MyVector::operator< (const MyVector& v) const{
     if(this->n!=v.n)                                
         return false; 
