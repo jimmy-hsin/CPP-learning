f9f78380990363501cf8e5f030be6b7ab302170c
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-19 21:49:33 +0800

    Update V12-13_globalize of Arithmetic operators.cpp

--- 1. Lecture/Week12_Operator_overloading/V12-13_globalize of Arithmetic operators.cpp
+++ 1. Lecture/Week12_Operator_overloading/V12-13_globalize of Arithmetic operators.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const MyVector operator+ (double d, const MyVector& v){
+	return v + d; // using the previous definition
+}
 
 class MyVector{
   private:
-    const int n;
+    int n;
     double* m;
   public:
   //Constructors
@@ -14,13 +18,20 @@ class MyVector{
     ~MyVector(){    delete [] m;    }
   //function
     void print() const;
+//Comparison Operator
     bool operator== (const MyVector& v) const; 
-    bool operator!= (const MyVector& v) const;
+    bool operator!= (const MyVector& v) const {    return !(*this == v);     }
     bool operator<  (const MyVector& v) const;
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~新增部分~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    double operator[] (int i) const;    //只是要取值的版本
-    double& operator[] (int i);         //有可能要更改值的版本，所以回傳值是元素所在的地址，且不宣告為const function
+//Assignment operator
+    double operator[] (int i) const;    
+    double& operator[] (int i);   
+    const MyVector& operator= (const MyVector& v);
+    const MyVector& operator+=(const MyVector& v);
+//Arithmetic operators
+    const MyVector& operator+ (const MyVector& v);
+    const MyVector& operator+ (double x);
 };
+//Constructor
 MyVector::MyVector(int n, double m[]):n(n){
     this->m=new double[n];  
     for(int i = 0; i < n; i++)
@@ -31,7 +42,7 @@ MyVector:: MyVector(const MyVector& v) : n(v.n){
     for(int i = 0; i < n; i++)
         m[i] = v.m[i];
 }
-
+//Functions
 void MyVector::print() const{   
     cout << "(";
     for(int i = 0; i < n - 1; i++)
@@ -46,9 +57,6 @@ bool MyVector::operator==(const MyVector& v) const{
             return false;          
     return true;
 }
-bool MyVector::operator!= (const MyVector& v) const{
-    return !(*this == v);     
-}
 bool MyVector::operator< (const MyVector& v) const{
     if(this->n!=v.n)                                
         return false; 
@@ -57,36 +65,55 @@ bool MyVector::operator< (const MyVector& v) const{
             return false;          
     return true;
 }
-//~~~~~~~~~~~~~~~~~~~~~~~~~~Overloading of index operator: [] ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//只回傳值的function
 double MyVector::operator[] (int i) const{
-	cout<<"const called"<<endl;
     if(i < 0 || i >= n)
-        exit(1); // terminate the program!  這是一個在<cstdlib> 裡定義的函數，他會終止程式並返回狀態碼 1
+        exit(1); 
     return m[i];
 }
-//可以改值的function
 double& MyVector::operator[] (int i) {
-	cout<<"non-const called"<<endl;
     if(i < 0 || i >= n)
         exit(1);
     return m[i];
 }
 
+const MyVector& MyVector::operator= (const MyVector& v){ 
+    if(*this == v)      return *this;
+    if(this->n != v.n){        //如果兩的大小不一樣
+        delete [] this->m;     //那就要把原本空間release，重新調整所需大小
+        this->n = v.n;
+        if(v.n == 0)    this->m = nullptr;
+        else            this->m=new double[this->n];
+    }
+    for(int i = 0; i < n; i++)
+        this->m[i] = v.m[i];
+    return *this;
+}
+const MyVector& MyVector::operator+= (const MyVector& v){
+    if(this->n == v.n)
+        for(int i = 0; i < n; i++)
+            this->m[i] += v.m[i];  
+    return *this;
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const MyVector MyVector::operator+ (const MyVector& v){ //回傳一個實體而不是reference是因為如果回傳reference的話，sum 的生命週期只會在這個function裡面，
+											  //之後就會被消滅，那之後再對function的結果做其他操作會容易出錯
+	MyVector sum(*this);//用copy constructor 複製出一個物件
+	sum+=v; 		//再用+=操作把sum跟v 相加之後回傳sum自己
+	return sum;
+}
+const MyVector operator+ (double x){
+    MyVector sum(*this);
+    for(int i=0;i<n;i++)
+        sum.m[i]+=x;
+    return sum;
+}
+
 int main() // with overloading
 {
-//-----------------Part 1-------------------------
     double d1[5] = {1, 2, 3, 4, 5};
-	MyVector a1(5, d1); 
-	cout << a1[3] << endl; 
-	a1[1] = 4; // 如果我們沒有call reference 的index overload 版本，那麼a1[1]就只是一個值，相當於對程式說 2=3，左邊只是值而不是容器，當然就會出錯
-//-----------------Part 2-------------------------
-	double d2[4] = {1, 2, 3, 4};
-	MyVector a2(4, d2); 
-	const MyVector a3(a1); //把a3 宣告成 constant object
-	a2[0] = 999;	//這邊呼叫一次non-const []
-	if(a1 == a3)
-		cout<< a2[0] << ", " << a3[0]; ////這邊a2又呼叫一次non-const [], 但a3 呼叫的是const []的版本
-	//如果我們沒有要修改MyVector的物件的值，那麼在沒有non-const []版本的情況下， a2、a3都會去呼叫const []的版本
+	MyVector a1(5, d1);
+	MyVector a3(a1);
+	a3 = 3 + a1 + 4 + a3;
+	a3.print();
 	return 0;
 }
