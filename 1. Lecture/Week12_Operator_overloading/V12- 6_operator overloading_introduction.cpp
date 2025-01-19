dc81993bb5c3b5333174935f87586ac50e1be0ef
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-19 17:38:32 +0800

    Update and rename V12- 6 .cpp to V12- 6_operator overloading_introduction.cpp

--- 1. Lecture/Week12_Operator_overloading/V12- 6_operator overloading_introduction.cpp
+++ 1. Lecture/Week12_Operator_overloading/V12- 6_operator overloading_introduction.cpp
@@ -7,13 +7,13 @@ class MyVector{
     double* m;
   public:
   //Constructors
-    MyVector(int n=0,double m[]=nullptr);//self define constructor
-    MyVector(const MyVector& v);//copy constructor
+    MyVector(int n=0,double m[]=nullptr); //self define constructor，(default constructor include version)
+    MyVector(const MyVector& v);          //copy constructor
   //destructor
     ~MyVector(){    delete [] m;    }
   //function
     void print() const;
-    bool operator==(const MyVector& v) const; //基本上就是把isEqual 的部分換成 operator==，'=='是我們想要overload的操作子
+    bool operator== (const MyVector& v) const; //基本上就是把isEqual 的部分換成 operator==，其中'=='是我們想要overload的操作子
 };
 MyVector::MyVector(int n, double m[]):n(n){
     this->m=new double[n];  
@@ -49,9 +49,11 @@ int main() // with overloading
     double d2[4] = {1, 2, 3, 4};
     const MyVector a2(4, d2);
     const MyVector a3(a1);
-    
+  
+    //現在我們可以直接把 == 作為一般操作子來用了
     cout << (a1 == a2 ? "Y" : "N") << "\n";
-    cout << (a1 == a3 ? "Y" : "N") << "\n";
+    //我們在做的事，其實是定義了一個名為.operator==()的function，只不過c++ 允許你用==來呼叫這個function 
+    cout << (a1.operator==(a3) ? "Y" : "N") << "\n";
     return 0;
 }
 
