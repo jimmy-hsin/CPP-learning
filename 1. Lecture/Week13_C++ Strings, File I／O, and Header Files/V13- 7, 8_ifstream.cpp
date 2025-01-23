5779dc5b9b2e5e5b89b9ae230480f7494e4621e9
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-23 09:17:57 +0800

    Update V13- 7, 8_ifstream.cpp

--- 1. Lecture/Week13_C++ Strings, File I／O, and Header Files/V13- 7, 8_ifstream.cpp
+++ 1. Lecture/Week13_C++ Strings, File I／O, and Header Files/V13- 7, 8_ifstream.cpp
@@ -6,13 +6,14 @@ using namespace std;
 int main()
 {
     ifstream inFile("score.txt");// 創建一個ifstream 的物件
+    //每個檔案的結尾其實存著一個特殊符號 "EOF" (end of file)
     if(inFile){
         string name;
         int score = 0;
         int sumScore = 0;
         int scoreCount = 0;
     
-        while(inFile >> name >> score){
+        while(inFile >> name >> score){    //這段就跟cin的邏輯差不多，會把遇到第一個空格(或換行、tab)之前的資料存入name, 第二個空格之前的資料存入score
             sumScore += score;
             scoreCount++;
         }
@@ -25,7 +26,7 @@ int main()
         cout<<"no file!";
     inFile.close();
     //Part 2
-    while(!inFile.eof()){
+    while(!inFile.eof()){    //判斷讀寫頭指的位置是不是EOF
         inFile >> name;
         inFile >> score;
         sumScore += score;
