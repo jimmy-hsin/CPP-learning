7f0777685290e6bf0f5664067392c49942d5ab8f
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-23 11:31:27 +0800

    Update main.cpp

--- 1. Lecture/Week13_C++ Strings, File I／O, and Header Files/V13-11_self define library/main.cpp
+++ 1. Lecture/Week13_C++ Strings, File I／O, and Header Files/V13-11_self define library/main.cpp
@@ -2,6 +2,14 @@
 #include "myMax.h"
 using namespace std;
 
+/*
+  要把自己的class變成一個self define library要先做以下事情
+  1. 把除了之前寫class時，member function的實作方式儲存進myClass.cpp( source file )裡，include header file 的檔名/位置
+  2. 創建一個header file: myClass.h, 其中包含所有class中的 member variable 和 member function 的definition寫進去，
+  3. 將以上兩個檔案一起compile
+*/
+
+
 int main() 
 {
   int a[LEN] = {7, 2, 5, 8, 9};
